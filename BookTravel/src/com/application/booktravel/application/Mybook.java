c42354efc847e170229034759ba124ec10c18d11
zwxhnu <2534948091@qq.com>
2016-05-08 23:28:39 +0800

    By wc

---
+++ BookTravel/src/com/application/booktravel/application/Mybook.java
@@ -0,0 +1,17 @@
+package com.application.booktravel.application;
+
+public class Mybook {
+
+	public Mybook() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
